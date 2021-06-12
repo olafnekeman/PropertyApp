2403011b0ac537ef9af926e39b4de41a98c056bd
Olaf Nekeman <olafnekeman@gmail.com>
2021-06-12 21:09:55 +0200

    First commit. A basic app

---
+++ support/constants.py
@@ -0,0 +1,6 @@
+""" This file holds all constants. """
+import os
+
+BASEPATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+MAPKEY = "pk.eyJ1Ijoib2xhZm5la2VtYW4iLCJhIjoiY2twdTQ5MHd4MHdzYjJub2N5bWM2Zm13OSJ9.mtwR2-3WRKut8Qed3lmryw"
